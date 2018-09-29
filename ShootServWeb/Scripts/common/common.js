002ad2edaa0f8b588aa4f660cfedc008578f20ab
d.levitin <disa15@rambler.ru>
2018-09-29 22:17:34 +0300

    отображение валидационных сообщений на форме

---
+++ ShootServWeb/Scripts/common/common.js
@@ -0,0 +1,22 @@
+﻿"use strict";
+(function () {
+    window.common = {};
+    var createVisualError = function (errorInput, message) {
+        errorInput.css("border-color","red");
+        alert(message);
+    }
+
+
+    window.common.lightValidationMessagesOnForm = function (form, validationMessages) {
+        $.each(validationMessages, function (key, value) {
+            var attribute = '[name=\"' + key +'\"]';
+            var errorInput = form.find(attribute);
+            if (errorInput) {
+                createVisualError(errorInput, value);
+                setInterval(function () {
+                    errorInput.attr('style','');
+                }, 20000)
+            }
+        })
+    }
+})();
