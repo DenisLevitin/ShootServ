580970c5cb453f0876aefb4b9e77584ae901119b
d.levitin <d.levitin@corp.youdo.com>
2018-12-24 18:23:53 +0300

    layout.js

--- ShootServWeb/Scripts/common/common.js
+++ ShootServWeb/Scripts/common/common.js
@@ -6,7 +6,6 @@
         alert(message);
     }
 
-
     window.common.lightValidationMessagesOnForm = function (form, validationMessages) {
         $.each(validationMessages, function (key, value) {
             var attribute = '[name=\"' + key +'\"]';
@@ -15,8 +14,8 @@
                 createVisualError(errorInput, value);
                 setInterval(function () {
                     errorInput.attr('style','');
-                }, 20000)
+                }, 20000);
             }
-        })
+        });
     }
 })();
---
+++ ShootServWeb/Scripts/common/layout.js
@@ -0,0 +1,15 @@
+﻿ //функция вызова ошибки
+function showError(text) {
+    // notImplemented
+}
+
+function showInfo(text) {
+    // notImplemented
+}
+
+function redirectToLoginPage(returnUrl) 
+{
+    if (confirm("Для данного действия требуется выполнить вход. Перейти на страницу авторизации?")) {
+        window.location = window.linkslogin + "?ReturnUrl=" + returnUrl;
+    }
+}
