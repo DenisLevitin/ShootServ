86b38b4494e91ded455928ba5a60f0b8ff07b50d
Levitina Veronika <levitinava@gmail.com>
2019-02-19 15:32:22 +0300

    загрузка нужных тиров

--- ShootServWeb/Scripts/ShootServ/shootingRangeList.js
+++ ShootServWeb/Scripts/ShootServ/shootingRangeList.js
@@ -37,29 +37,23 @@ $(document).ready(function () {
     });
 });
 
-var shootingRangeListActor = function () {
-    
-    // Нужно вызвать эту функцию при изменении региона
-    this.changeRegion = function () {
-        var idRegion = $("#region-choise").val();
-        this.getListByRegion(idRegion);
-    };
 
-    this.construct = function(){
-    };
-    
+
+
+
     // Получить список тиров по региону
     this.getListByRegion = function(idRegion)
     {
+        var rez;
         if (idRegion) {
             $.ajax({
                 url: linksShootingRange.GetListByRegion,
-                dataType: "html",
+                dataType: "json",
                 data: { idRegion: idRegion },
                 async: false,
                 success: function (data) {
                     /// TODO: в data json, надо построить в listShootingRanges таблицу
-                    $("#listShootingRanges").html(data);
+                    rez = data;
                 },
                 error: function ()
                 {
@@ -67,5 +61,48 @@ var shootingRangeListActor = function () {
                 }
             });
         }
+        return rez;
+};
+
+    //таблица JQGrid
+//очистить
+this.clearTable = function () {
+    $(".gridContainer").html('<table id="list"><tr><td></td></tr></table>');
+};
+//создать
+    this.createGrid = function (ranges) {
+        clearTable();
+        $("#list").jqGrid({
+            datatype: "local",
+            data: ranges,
+            colNames: ['Тир', 'Телефон', 'Регион', 'Адрес'],
+            colModel: [
+                { name: 'Name', "label": 'Name', "sortable": true },
+                { name: 'Phone', "label": 'phone', "sortable": false },
+                { name: 'RegionName', "label": 'RegionName', "sortable": true, "sorttype": "string" },
+                { name: 'Address', "sortable": false }
+            ],
+            styleUI: 'Bootstrap',
+            rowNum: 10,
+            rowList: [10, 20, 30],
+            gridview: true,
+            autoencode: true,
+            viewrecords: true,
+            height: 250,
+            autoWidth: true
+        });
+    };
+var shootingRangeListActor = function () {
+
+    // Нужно вызвать эту функцию при изменении региона
+    this.changeRegion = function () {
+        var idRegion = $("#region-choise").val();
+        if (idRegion) {
+            var rages = getListByRegion(idRegion);
+            createGrid(rages);
+        }
+        };
+
+    this.construct = function () {
     };
 };
