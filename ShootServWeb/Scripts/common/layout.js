bd0b3e3533b73243abe20605fef4d406cf8f9440
Levitina Veronika <levitinava@gmail.com>
2019-02-27 14:21:16 +0300

    add modal: ShowError showInfo showConfirm

--- ShootServWeb/Scripts/common/layout.js
+++ ShootServWeb/Scripts/common/layout.js
@@ -1,15 +1,37 @@
 ﻿ //функция вызова ошибки
 function showError(text) {
-    alert(text);
+    $('#showErrorModal').modal('show');
+    $('#showErrorModal').on('shown.bs.modal', function () {
+        var modal = $(this);
+        modal.find('.modal-body').text(text);
+    });
 }
 
-function showInfo(text) {
-    alert(text);
+function showInfo(text, callback) {
+  
+    $('#showInfoModal').modal('show');
+    $('#showInfoModal').on('shown.bs.modal', function () {
+        var modal = $(this);
+        modal.find('.modal-body').text(text);
+    });
+    $('#showInfoModal').on('hide.bs.modal', function () {
+        callback();
+    });
 }
+
+var showConfirm = function (text, returnUrl) {
+    $('#showConfirmModal').modal('show');
+    $('#showConfirmModal').on('shown.bs.modal', function () {
+        var modal = $(this);
+        modal.find('.modal-body').text(text);
+    });
+   $('#modalAgree').on('click', function () {
+        window.location = window.linkslogin + "?ReturnUrl=" + returnUrl;
+    });
+};
 
 function redirectToLoginPage(returnUrl) 
 {
-    if (confirm("Для данного действия требуется выполнить вход. Перейти на страницу авторизации?")) {
-        window.location = window.linkslogin + "?ReturnUrl=" + returnUrl;
-    }
+    showConfirm("Для данного действия требуется выполнить вход. Перейти на страницу авторизации?");
+
 }
