74f8e2c772a0b92e201bb34667425c39e481b7e8
Levitina Veronika <levitinava@gmail.com>
2019-03-06 11:11:12 +0300

    страница профиля

---
+++ ShootServWeb/Scripts/ShootServ/profile.js
@@ -0,0 +1,16 @@
+﻿
+$(document).ready( function() {
+    var userData = $(".js-user-data");
+    $("#editButton").on('click', function () {
+        if (userData.hasClass("no-edit")) {
+            userData.removeClass("no-edit");
+            $(".js-user-data .form-control").prop('disabled', false);
+            $("#submitButton").removeClass("hidden");
+        } else {
+            userData.addClass("no-edit");
+            $(".js-user-data .form-control").prop('disabled', true);
+            $("#submitButton").addClass("hidden");
+        }
+    });
+});
+
