924d6f6bd5d0a9b09605f7da512981f79c7c4501
Levitina Veronika <levitinava@gmail.com>
2019-03-06 12:16:31 +0300

    fix  отмена редактирования

--- ShootServWeb/Scripts/ShootServ/profile.js
+++ ShootServWeb/Scripts/ShootServ/profile.js
@@ -1,16 +1,36 @@
 ﻿
 $(document).ready( function() {
     var userData = $(".js-user-data");
-    $("#editButton").on('click', function () {
+    var editButton = $("#editButton");
+    var data;
+    var addStartData = function (data) {
+        var currentInputName;
+        for (var i = 0; i < data.length; i++) {
+            currentInputName = data[i].name;
+            $("input[name*='" + currentInputName + "']").prop('value', data[i].value);
+        }
+    };
+
+
+    editButton.on('click', function () {
         if (userData.hasClass("no-edit")) {
             userData.removeClass("no-edit");
             $(".js-user-data .form-control").prop('disabled', false);
+            data = $(".js-upload-user-data").serializeArray()
             $("#submitButton").removeClass("hidden");
-        } else {
+            editButton.prop("value", "Отменить редактирование");
+        } else { 
+
+            if (data) {
+                addStartData(data.slice());
+            }
+            editButton.prop("value", "Редактирование");
             userData.addClass("no-edit");
-            $(".js-user-data .form-control").prop('disabled', true);
             $("#submitButton").addClass("hidden");
+            $(".js-user-data .form-control").prop('disabled', true);
         }
     });
+
+
 });
 
